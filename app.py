097c8140538210c1ac87b375e15d0dd7fb2a304b
Woshte <info9242ngumujobs@gmail.com>
2024-12-22 22:19:55 +0300

    html

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request
+from weather import main as get_weather
+
+app = flask(__name__)
+
+@app.route('/')
+def index.html
+
+if __name__=='__main__':
+    app.run(debug=True)
+      
